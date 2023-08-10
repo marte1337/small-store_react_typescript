787283e357e41fca9801a936ea866b305bc403a4
marte1337 <54943747+marte1337@users.noreply.github.com>
2023-08-10 11:44:49 +0200

    feature: currency formatter

--- src/utitlities/formatCurrency.ts
+++ src/utitlities/formatCurrency.ts
@@ -1,1 +1,8 @@
-export function formatCurrency(number: number) {}
+const CURRENCY_FORMATTER = new Intl.NumberFormat(undefined, {
+  currency: "USD",
+  style: "currency",
+});
+
+export function formatCurrency(number: number) {
+  return CURRENCY_FORMATTER.format(number);
+}
